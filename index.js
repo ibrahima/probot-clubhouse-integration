2fc1488f148948d3608d253469cb08ea85da345d
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2017-12-03 20:49:48 -0800

    Pull Clubhouse story ID out of branch name

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 module.exports = (robot) => {
+  const storyRe = /ch\d+/;
   console.log('Yay, the app was loaded!');
 
   robot.on('pull_request.labeled', async context => {
     robot.log.info('Pull request labeled!');
-    const name = context.payload.label.name;
-    robot.log(name);
+    const labelName = context.payload.label.name;
+    const branchName = context.payload.pull_request.head.ref;
+    const storyId = storyRe.exec(branchName);
+    robot.log(labelName, branchName, storyId);
   });
 
   robot.on('pull_request.unlabeled', async context => {
     robot.log.info('Pull request unlabeled!');
-    const name = context.payload.label.name;
-    robot.log(name);
+    const labelName = context.payload.label.name;
+    const branchName = context.payload.pull_request.head.ref;
+    const storyId = storyRe.exec(branchName);
+    robot.log(labelName, branchName, storyId);
   });
 };
